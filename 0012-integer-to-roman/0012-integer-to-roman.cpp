c50edc24c6472dc5be1c6c779c5fa6777828d78b
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-26 12:01:37 +0530

    Time: 0 ms (100%), Space: 12.2 MB (48.87%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string ans = "";
+
+        for (int i = 0; i < value.size(); i++) {
+            while (num >= value[i]) {
+                ans += symbol[i];
+                num -= value[i];
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
