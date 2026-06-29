0ff0280b0ac53dd6313fbea0c89b5b5f6274895c
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 15:16:10 +0530

    Time: 4 ms (94.87%), Space: 14 MB (40.13%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        int ans = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - ans))
+                    ans = sum;
+
+                if (sum < target)
+                    left++;
+                else if (sum > target)
+                    right--;
+                else
+                    return target;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
