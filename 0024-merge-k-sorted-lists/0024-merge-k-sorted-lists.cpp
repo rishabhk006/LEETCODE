814aded23a45f23720e45144fb030121484e1aa5
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-01 12:21:38 +0530

    Time: 4 ms (42.45%), Space: 18.3 MB (81.15%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0024-merge-k-sorted-lists/0024-merge-k-sorted-lists.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+
+        for (auto node : lists) {
+            if (node)
+                pq.push(node);
+        }
+
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next)
+                pq.push(node->next);
+        }
+
+        return dummy.next;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
