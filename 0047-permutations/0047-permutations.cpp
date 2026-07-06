25791907d99258ea4e33a06f358c84e6e187ad19
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-06 12:02:20 +0530

    Time: 0 ms (100%), Space: 10.9 MB (35.47%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0047-permutations/0047-permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            solve(nums, idx + 1);
+            swap(nums[idx], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums, 0);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
