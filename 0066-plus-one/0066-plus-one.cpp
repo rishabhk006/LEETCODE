fd8c9d3fad5500eac1536e56a535c3fc4f78ed65
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 15:14:45 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.94%) - LeetHub - Time: O, Space: O(?)

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
