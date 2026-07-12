1d764af7a3fd45c6cbcfdb80e462afa38c5a7d7f
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-12 23:39:29 +0530

    Time: 15 ms (11.31%), Space: 19.6 MB (60.5%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 2)
+            return nums.size();
+
+        int k = 2;
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
