89760f39c7bc7ecb7c291ba97f778105c3fa96ae
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-18 23:54:57 +0530

    Time: 0 ms (100%), Space: 12.9 MB (48.15%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
