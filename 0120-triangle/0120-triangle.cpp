d9cd763f76abea26b13f62e899d7451e1e6cefa9
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-22 14:44:47 +0530

    Time: 0 ms (100%), Space: 12.6 MB (93.15%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp = triangle.back();
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
