057e665a575ea53e5a9fe0a21254e48fde85d11d
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-22 14:40:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (74.76%) - LeetHub - Time: O(rowIndex^2), Space: O(rowIndex)

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1, 0);
+        row[0] = 1;
+
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i; j >= 1; j--) {
+                row[j] += row[j - 1];
+            }
+        }
+
+        return row;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
