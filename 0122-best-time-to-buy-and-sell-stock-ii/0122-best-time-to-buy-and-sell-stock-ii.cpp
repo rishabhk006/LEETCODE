4eb1efb8bd4f64b1a2ae8c22b915b46654346049
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-22 14:46:43 +0530

    Time: 0 ms (100%), Space: 19.9 MB (97.82%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+
+        return profit;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
