87902b057818f16a6887452c531ece515f6b1076
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-08-06 11:38:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.77%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int digitProduct(int x) {
+        int product = 1;
+        while (x > 0) {
+            product *= (x % 10);
+            x /= 10;
+        }
+        return product;
+    }
+
+    int smallestNumber(int n, int t) {
+        while (true) {
+            if (digitProduct(n) % t == 0)
+                return n;
+            n++;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
